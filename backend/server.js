79cbee222ee66349f61cee70d8838f7f63c2828a
Nizar Beriane <nizarberiane@proton.me>
2025-09-19 09:28:04 +0100

    Remove duplicate log statement in `server.js`

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
 const express = require('express');
 const app = express();
 app.listen(Bun.env.PORT, () => console.log('Server is running on port 3000'));
+console.log('Server is running on port 3000');
