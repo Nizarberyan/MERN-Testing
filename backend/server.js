8dcc85c677ce83f022ebe6cdf56efb18db717fac
Nizar Beriane <nizarberiane@proton.me>
2025-09-19 09:13:15 +0100

    Update `.gitignore` for backend and refactor `server.js` to use `Bun.env.PORT`

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,3 @@
 const express = require('express');
-require('dotenv').config();
 const app = express();
-app.listen(process.env.PORT, () => console.log('Server is on port 3000'));
-app.get('/', (req, res) => res.json({message:'Hello, World!'}));
+app.listen(Bun.env.PORT, () => console.log('Server is running on port 3000'));
